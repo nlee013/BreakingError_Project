aadb040bffe723e477e28ab2f2b1e836054eead9
nlee013 <valuenacreator@gmail.com>
2021-05-06 16:57:46 +0900

    Finished Main, Load, Play, Game scenes

---
+++ BreakingError2/Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene("Play");
+        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("MainGame");
+
+    }
+}
